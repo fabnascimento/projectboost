8065bd4ad3bc1b06d8b287804e37aa5ea5e6236e
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2024-07-01 23:58:25 -0400

    creating crash and complete level functions

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,18 +1,30 @@
-extends Node3D
+extends RigidBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var number: int = 10
-	print(number)
-
-
+@export_range(750, 2000) var thrust: float = 1000
+@export_range(50, 750) var torque_force: float = 100
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += 1 * delta
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
 	
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0, 0.0, torque_force * delta))
 
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, -torque_force * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "goal" in body.get_groups():
+		complete_level()
+	
+	if "obstacle" in body.get_groups():
+		crash_sequence()
+
+func crash_sequence() -> void:
+	print('eita')
+	get_tree().reload_current_scene()
+
+func complete_level() -> void:
+	print("completou")
+	get_tree().quit()
